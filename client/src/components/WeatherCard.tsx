07a90df3a644fff7a4e42ea1cb74b27ccff7005b
Sasha Ctp <sasha.contrepois@hotmail.fr>
2022-06-14 20:15:36 +0200

    Fix CORS issue

--- client/src/components/WeatherCard.tsx
+++ client/src/components/WeatherCard.tsx
@@ -5,18 +5,21 @@ import '../style/card.css';
 const WeatherCard = () => {
 
     const [weather, setWeather] = useState([]);
-    var myHeaders = new Headers({
-        "Accept" : "application/json",
-        "Access-Control-Allow-Origin" : "http://localhost:3000/"
-      });
+
+    // TODO - ⚠️ Setup a config file and change credentials ⚠️
+    const TOKEN = '617d81918aac72bde4dda3798e98c7f4bcad48caa8ff11beaf28bd45e7b39986';
+    const headers = {
+        'Accept' : 'application/json',
+    };
     
     useEffect(() => {
-        fetch('http://api.meteo-concept.com/api/location/city?token=617d81918aac72bde4dda3798e98c7f4bcad48caa8ff11beaf28bd45e7b39986&insee=35238', {
+        fetch(`http://api.meteo-concept.com/api/location/city?token=${TOKEN}&insee=35238`, {
             method: 'GET',
-            headers: myHeaders,
+            headers: headers,
         })
         .then(data => data.json())
         .then(data => console.log(data.city))
+        .catch(err => console.error(err));
     },[]);
     return (
         <Card icon='⛅' title='Weather'>
