97a23d452c4cdc855bb9cd8cd3774a4c03a69e36
Sasha Ctp <sasha.contrepois@hotmail.fr>
2022-06-15 16:46:10 +0200

    Add weather loader

--- client/src/components/Weather/WeatherCard.tsx
+++ client/src/components/Weather/WeatherCard.tsx
@@ -1,9 +1,11 @@
 import React, { Dispatch, useEffect, useState } from "react";
 import Card from "../Card/Card"
+import Loader from "../Loader/Loader";
 import './WeatherCard.css';
 
 const WeatherCard = () => {
     const [weatherData, setWeatherData]: [any, Dispatch<any>] = useState();
+    const [loaded, setLoaded] = useState(false);
     const getLocation = () => {
 
         let location = {
@@ -18,6 +20,8 @@ const WeatherCard = () => {
                     location.longitude = position.coords.longitude;
                     console.log(`Geolocation : ${location.latitude}, ${location.longitude}`);
                     resolve(location);
+                }, (err) => {
+                    reject(err);
                 });
             } else {
                 console.log("Geolocation is not supported by this browser.");
@@ -39,55 +43,70 @@ const WeatherCard = () => {
                     }
                 })
                 .then(data => data.json())
-                .then(data => {setWeatherData(data); console.log(data)})
-                .catch(err => console.error(err));
+                .then(data => {
+                    setWeatherData(data); 
+                    console.log(data);
+                    setLoaded(true);
+                })
+                .catch(err => {
+                    console.error(err);
+                    setLoaded(true);
+                });
         })
             .catch(err => console.error(err));
     }, []);
 
-return (
-    <Card icon='⛅' title='Weather'>
-        <div className="container">
-            <div className="temperature">
-                <h1>
-                    {weatherData ? weatherData?.forecast[0].temp2m + '°' : ''}
-                </h1>
-                <h3>
-                    {weatherData?.city.name}
-                </h3>
+    if(!loaded){
+        return (
+            <Card icon='⛅' title='Weather'>
+                <Loader text="Loading weather data ..."/>               
+            </Card>
+        )
+    }
+
+    return (
+        <Card icon='⛅' title='Weather'>
+            <div className="container">
+                <div className="temperature">
+                    <h1>
+                        {weatherData ? weatherData?.forecast[0].temp2m + '°' : 'No location given'}
+                    </h1>
+                    <h3>
+                        {weatherData?.city.name}
+                    </h3>
+                </div>
+                <div className="parameter">
+                    <div className="windspeed">
+                        <h3>
+                            Windspeed
+                        </h3>
+                        <h1>
+                            {weatherData ? weatherData?.forecast[0].wind10m : '-'} km/h
+                        </h1>
+
+                    </div>
+                    <div className="humidity">
+                        <h3>
+                            Humidity
+                        </h3>
+                        <h1>
+                            {weatherData ? weatherData?.forecast[0].rh2m : '-'} %
+                        </h1>
+
+                    </div>
+                    <div className="precipitation">
+                        <h3>
+                            Precipitation probability
+                        </h3>
+                        <h1>
+                            {weatherData ? weatherData?.forecast[0].probarain : '-'} %
+                        </h1>
+
+                    </div>
+                </div>
             </div>
-            <div className="parameter">
-                <div className="windspeed">
-                    <h3>
-                        Windspeed
-                    </h3>
-                    <h1>
-                        {weatherData?.forecast[0].wind10m + ' km/h'}
-                    </h1>
-
-                </div>
-                <div className="humidity">
-                    <h3>
-                        Humidity
-                    </h3>
-                    <h1>
-                        {weatherData?.forecast[0].rh2m + ' %'}
-                    </h1>
-
-                </div>
-                <div className="precipitation">
-                    <h3>
-                        Precipitation probability
-                    </h3>
-                    <h1>
-                        {weatherData?.forecast[0].probarain + ' %'}
-                    </h1>
-
-                </div>
-            </div>
-        </div>
-    </Card>
-)
+        </Card>
+    );
 
 };
 
